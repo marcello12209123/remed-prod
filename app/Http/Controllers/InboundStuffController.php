276ef3bdae96c27e707f138557b666afb68ffac4
marcello12209123 <marcelloimmanuellitano@smkwikrama.sch.id>
2024-04-22 12:14:18 +0700

    update prod

--- app/Http/Controllers/InboundStuffController.php
+++ app/Http/Controllers/InboundStuffController.php
@@ -2,30 +2,52 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\InboundStuff;
-use Illuminate\Http\Request;
-use App\Models\Stuff;
-use App\Models\StuffStock;
 use App\Helpers\ApiFormatter;
+use App\Models\InboundStuff;
+use App\Models\StuffStock;
+use App\Models\Stuff;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\App;
+
 
 class InboundStuffController extends Controller
 {
-    public function index()
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index( request $request)
     {
         try{
-            $data = InboundStuff::with('stuff')->get();
-    
+            if($request->filter_id){
+               $data = InboundStuff::where('stuff_id', $request->filter_id)->with('stuff','stuff.stuffStock')->get();
+            }else{
+                $data = InboundStuff::all();
+            }
             return Apiformatter::sendResponse(200, 'succes', $data);
            }catch(\Exception $err){
-            return ApiFormatter::sendResponse(400, 'bad request', $err->getMessage());
+            return ApiFormatter::sendResponse(400, 'bad request',$err->getMessage());
            }
     }
 
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function create()
     {
         //
     }
 
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
     public function store(Request $request)
     {
         try {
@@ -33,123 +55,211 @@ class InboundStuffController extends Controller
                 'stuff_id' => 'required',
                 'total' => 'required',
                 'date' => 'required',
-                'proff_file' => 'required|mimes:jpeg,png,pdf|max:2048',
+                'proff_file' => 'required|mimes:jpeg,png,jpg,pdf|max:2048',
             ]);
 
-            if ($request->hasFile('proff_file')) {
-                $proof = $request->file('proff_file');
-                $destinationPath = 'proof/';
-                $proofName = date('YmdHis') . "." . $proof->getClientOriginalExtension();
-                $proof->move($destinationPath, $proofName);
+            if($request->hasFile('proff_file')) {
+                $proff = $request->file('proff_file');
+                $destinationPath = 'proff/';
+                $proffName = date('YmdHis') . "." . $proff->getClientOriginalExtension();
+                $proff->move($destinationPath, $proffName);
             }
-
             $createStock = InboundStuff::create([
                 'stuff_id' => $request->stuff_id,
-                'total' => $request->stuff_id,
+                'total' => $request->total,
                 'date' => $request->date,
-                'proff_file' => $proofName,
+                'proff_file' => $proffName,
             ]);
 
-            if ($createStock) {
+            if ($createStock){
                 $getStuff = Stuff::where('id', $request->stuff_id)->first();
                 $getStuffStock = StuffStock::where('stuff_id', $request->stuff_id)->first();
 
-                if (!$getStuffStock) {
+                if (!$getStuffStock){
                     $updateStock = StuffStock::create([
                         'stuff_id' => $request->stuff_id,
                         'total_available' => $request->total,
                         'total_defec' => 0,
-
                     ]);
-
-                }else {
-                    $updateStock = $getstuffStock->update([
+                } else {
+                    $updateStock = $getStuffStock->update([
                         'stuff_id' => $request->stuff_id,
-                        'total_available' => $getStuffStock['total_available'] + $request->total,
+                        'total_available' =>$getStuffStock['total_available'] + $request->total,
                         'total_defec' => $getStuffStock['total_defec'],
                     ]);
-
                 }
 
                 if ($updateStock) {
-                    $getStock = stuffStock::where('stuff_id', $request->stuff_id)->first();
+                    $getStock = StuffStock::where('stuff_id', $request->stuff_id)->first();
                     $stuff = [
                         'stuff' => $getStuff,
                         'InboundStuff' => $createStock,
-                        'stuffStock' => $getStock,
-
+                        'stuffStock' => $getStock
                     ];
-                    return APIFormatter::sendResponse(200, 'successfully Createa a Inbound Stuff Data', $stuff);
 
-                }else{
-                    return APIFormatter::sendResponse(400, 'failed to update a stuff Stock data');
+                    return ApiFormatter::sendResponse(200, 'Successfully Create A Inbound Stuff Data', $stuff);
+                } else {
+                    return ApiFormatter::sendResponse(400, false, 'Failed To Update A Stuff Stock Data');
                 }
             } else {
-                return APIFormatter::sendResponse(400, 'failed to create a inbund stuff data');
             }
-
-        }catch (\Throwable $th) {
-            return APIFormatter::sendResponse(400, false, $th->getMessage());
-
+        } catch (\Exception $e) {
+            return ApiFormatter::sendResponse(400, false, $e->getMessage());
         }
     }
 
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\InboundStuff  $inboundStuff
+     * @return \Illuminate\Http\Response
+     */
     public function show(InboundStuff $inboundStuff)
     {
-        try {
-            $data = InboundStuff::with('stuff')->where('id', $id)->first();
-
-            if (is_null($data)) {
-                return ApiFormatter::sendResponse(400, 'bad request', 'Data not found!');
-            } else {
-                return ApiFormatter::sendResponse(200, 'success', $data);
-            }
-        } catch (\Exception $err) {
-            return ApiFormatter::sendResponse(400, 'bad request', $err->getMessage());
-        }
+        //
     }
 
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\InboundStuff  $inboundStuff
+     * @return \Illuminate\Http\Response
+     */
     public function edit(InboundStuff $inboundStuff)
     {
         //
     }
 
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\InboundStuff  $inboundStuff
+     * @return \Illuminate\Http\Response
+     */
     public function update(Request $request, InboundStuff $inboundStuff)
     {
-        try {
-            $this->validate($request, [
-                'username' => 'required',
-                'email' => 'required',
-                'password' => 'required',
-                'role' => 'required',
-            ]);
-
-            $checkProses = User::where('id', $id)->update([
-                'username' => $request->username,
-                'email' => $request->email,
-                'password' => $request->password,
-                'role' => $request->role,
-            ]);
-
-            if($checkProses) {
-                $data = User::find($id);
-                return ApiFormatter::sendResponse(200, 'success', $data);
-            } else {
-                return ApiFormatter::sendResponse(400, 'bad request', 'Gagal mengubah data! ');
-            }
-        } catch (\Exception $err) {
-            return ApiFormatter::sendResponse (400, 'bad request', $err->getMessage());
-            }
+        //
     }
 
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\InboundStuff  $inboundStuff
+     * @return \Illuminate\Http\Response
+     */
     public function destroy(InboundStuff $inboundStuff)
     {
-        try{
-            $checkProses= InboundStuff::where('id', $id)->delete();
+        try {
+            $checkProses = InboundStuff::where('id', $id)->first();
 
-            return ApiFormatter::sendResponse(200, 'success', 'Data stuff berhasil di hapus');
+            if ($checkProses) {
+                $stuffId = $checkProses->stuff_id;
+                $totalInbound = $checkProses->total;
+                $checkProses->delete();
+
+                $dataStock = StuffStock::where('stuff_id', $checkProses->stuff_id)->first();
+
+                if ($dataStock) {
+                    $total_available = (int)$dataStock->total_available - (int)$totalInbound;
+                    $minusTotalStock = $dataStock->update(['total_available' => $total_available]);
+
+                    if ($minusTotalStock) {
+                        $updateStufAndInbound = Stuff::where('id', $stuffId)->with('inboundStuffs', 'stuffStock')->first();
+                        return ApiFormatter::sendResponse(200, 'success', $updateStufAndInbound);
+                    }
+                } else {
+                    // Tangani jika data stok tidak ditemukan
+                    return ApiFormatter::sendResponse(404, 'not found', 'Data stok stuff tidak ditemukan');
+                }
+            } else {
+                // Tangani jika data InboundStuff tidak ditemukan
+                return ApiFormatter::sendResponse(404, 'not found', 'Data InboundStuff tidak ditemukan');
+            }
+        } catch (\Exception $err) {
+            // Tangani kesalahan
+            return ApiFormatter::sendResponse(400, 'bad request', $err->getMessage());
+        }
+    }
+
+    public function trash()
+    {
+        try{
+            $data= InboundStuff::onlyTrashed()->get();
+
+            return ApiFormatter::sendResponse(200, 'success', $data);
         }catch(\Exception $err){
             return ApiFormatter::sendResponse(400, 'bad request', $err->getMessage());
+        }
+    }
+
+    public function restore(InboundStuff $inboundStuff, $id)
+    {
+        try {
+            // Memulihkan data dari tabel 'inbound_stuffs'
+            $checkProses = InboundStuff::onlyTrashed()->where('id', $id)->restore();
+
+            if ($checkProses) {
+                // Mendapatkan data yang dipulihkan
+                $restoredData = InboundStuff::find($id);
+
+                // Mengambil total dari data yang dipulihkan
+                $totalRestored = $restoredData->total;
+
+                // Mendapatkan stuff_id dari data yang dipulihkan
+                $stuffId = $restoredData->stuff_id;
+
+                // Memperbarui total_available di tabel 'stuff_stocks'
+                $stuffStock = StuffStock::where('stuff_id', $stuffId)->first();
+
+                if ($stuffStock) {
+                    // Menambahkan total yang dipulihkan ke total_available
+                    $stuffStock->total_available += $totalRestored;
+
+                    // Menyimpan perubahan pada stuff_stocks
+                    $stuffStock->save();
+                }
+
+                return ApiFormatter::sendResponse(200, 'success', $restoredData);
+            } else {
+                return ApiFormatter::sendResponse(400, 'bad request', 'Gagal mengembalikan data!');
+            }
+        } catch (\Exception $err) {
+            return ApiFormatter::sendResponse(400, 'bad request', $err->getMessage());
+        }
+    }
+
+
+    public function deletePermanent(InboundStuff $inboundStuff, Request $request, $id)
+    {
+        try {
+            $getInbound = InboundStuff::onlyTrashed()->where('id',$id)->first();
+
+            unlink(base_path('public/proff/'.$getInbound->proff_file));
+            // Menghapus data dari database
+            $checkProses = InboundStuff::where('id', $id)->forceDelete();
+
+            // Memberikan respons sukses
+            return ApiFormatter::sendResponse(200, 'success', 'Data inbound-stuff berhasil dihapus permanen');
+        } catch(\Exception $err) {
+            // Memberikan respons error jika terjadi kesalahan
+            return ApiFormatter::sendResponse(400, 'bad request', $err->getMessage());
+        }
+    }
+
+    private function deleteAssociatedFile(InboundStuff $inboundStuff)
+    {
+        // Mendapatkan jalur lengkap ke direktori public
+        $publicPath = $_SERVER['DOCUMENT_ROOT'] . '/public/proff';
+
+
+        // Menggabungkan jalur file dengan jalur direktori public
+         $filePath = public_path('proff/'.$inboundStuff->proff_file);
+
+        // Periksa apakah file ada
+        if (file_exists($filePath)) {
+            // Hapus file jika ada
+            unlink(base_path($filePath));
         }
     }
 }
